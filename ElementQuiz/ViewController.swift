5442379438c4203f18d6a866a7eb4c1c70b146f7
Jon <jontierney@m4cb00k.local>
2019-03-05 18:59:16 -0500

    initial commit

--- ElementQuiz/ViewController.swift
+++ ElementQuiz/ViewController.swift
@@ -12,9 +12,60 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateQuiz()
+        startQuiz()
     }
 
-
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var mainStackView: UIStackView!
+    @IBOutlet weak var goAgainButton: UIButton!
+    
+    @IBAction func gotoNextQuiz(_ sender: Any) {
+        currentQuizIndex += 1
+        if currentQuizIndex >= quizList.count {
+            resetQuiz()
+        } else {
+            afterResetQuiz()
+        }
+        
+        updateQuiz()
+    }
+    
+    @IBAction func goAgainButtonClicked(_ sender: Any) {
+        afterResetQuiz()
+    }
+    
+    @IBAction func showAnswer(_ sender: Any) {
+        answerLabel.text = quizList[currentQuizIndex]
+    }
+    
+    let quizList = ["Carbon", "Gold", "Chlorine", "Sodium", "Oxygen", "Nitrogen", "Neon"]
+    
+    var currentQuizIndex = 0
+    
+    func startQuiz() {
+        mainStackView.isHidden = false
+        goAgainButton.isHidden = true
+    }
+    
+    func resetQuiz() {
+        goAgainButton.isHidden = false
+        goAgainButton.isEnabled = true
+        currentQuizIndex = 0
+        mainStackView.isHidden = true
+    }
+    
+    func afterResetQuiz() {
+        goAgainButton.isHidden = true
+        goAgainButton.isEnabled = false
+        mainStackView.isHidden = false
+    }
+    
+    func updateQuiz() {
+        answerLabel.text = "?"
+        let quizName = quizList[currentQuizIndex]
+        let image = UIImage(named: quizName)
+        imageView.image = image
+    }
 }
-
